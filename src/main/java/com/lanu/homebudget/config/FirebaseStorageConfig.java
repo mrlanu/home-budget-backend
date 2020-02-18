fc5e4105f44b6d5b6ca1e9405a52f0ef00fffb6c
mrlanu <mrlanu@gmail.com>
2020-02-17 20:36:24 -0600

    fix: FirebaseStorageConfig

--- src/main/java/com/lanu/homebudget/config/FirebaseStorageConfig.java
+++ src/main/java/com/lanu/homebudget/config/FirebaseStorageConfig.java
@@ -1,13 +1,9 @@
 package com.lanu.homebudget.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.common.collect.Lists;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.StorageClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,7 +19,7 @@ public class FirebaseStorageConfig {
         GoogleCredentials credentials = null;
         try {
             credentials = GoogleCredentials
-                    .fromStream(new FileInputStream("/home/lanu/development/MyProjects/Java-apps/home-budget/images-service-key.json"))
+                    .fromStream(new FileInputStream("src/main/resources/images-service.json"))
                     .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
         } catch (IOException e) {
             e.printStackTrace();
